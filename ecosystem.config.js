e2fbfa771554cc4e19487bf5cc7ef77237aee8b4
“bfcxfm” <bfcxfm@gmail.com>
2024-07-17 14:28:12 +0800

    Update PM2 configuration for EC2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [{
+      name: "bbt-backend",
+      script: "./server.js",
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000,
+        FRONTEND_URL: "http://localhost:5173"
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        FRONTEND_URL: "http://ec2-54-255-232-252.ap-southeast-1.compute.amazonaws.com"
+      }
+    }]
+  }
