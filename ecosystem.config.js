dc22d18b841fe729888a237ab53446f267ee573a
“bfcxfm” <bfcxfm@gmail.com>
2024-07-17 15:02:02 +0800

    update pm2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
       name: "bbt-backend",
-      script: "./server.js",
+      script: "./bin/www",
       env: {
         NODE_ENV: "development",
         PORT: 3000,
