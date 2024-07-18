2597d052aa1be0b9ff5a7460c35818922d661105
“bfcxfm” <bfcxfm@gmail.com>
2024-07-18 22:59:54 +0800

    Update updateProduct

--- controllers/users.js
+++ controllers/users.js
@@ -3,214 +3,209 @@ const product = require("../daos/product");
 const Users = require("../models/users");
 
 module.exports = {
-    signup,
-    loginDetails,
-    loginUser,
-    checkLogin,
-    checkPermission,
-    logoutUser,
-    createOrder,
-    orderDetails,
-    updateOrder,
-    productDetails,
-    createProduct,
-    allProductDetails,
-    allOrderDetails,
-    updateProduct
-}
+  signup,
+  loginDetails,
+  loginUser,
+  checkLogin,
+  checkPermission,
+  logoutUser,
+  createOrder,
+  orderDetails,
+  updateOrder,
+  productDetails,
+  createProduct,
+  allProductDetails,
+  allOrderDetails,
+  updateProduct,
+};
 
 async function signup(req, res) {
-    try {
-      const user = await Users.signup(req.body);
-      // Always redirect after CUDing data
-      // We'll refactor to redirect to the movies index after we implement it
-      // res.redirect('/movies/new'); SKIP old code
-      res.json(user)
-    } catch (err) {
-      // Typically some sort of validation error
-      console.log(err);
-      // res.render('movies/new', { errorMsg: err.message }); SKIP old code
-      res.status(500).json({ err });
+  try {
+    const user = await Users.signup(req.body);
+    // Always redirect after CUDing data
+    // We'll refactor to redirect to the movies index after we implement it
+    // res.redirect('/movies/new'); SKIP old code
+    res.json(user);
+  } catch (err) {
+    // Typically some sort of validation error
+    console.log(err);
+    // res.render('movies/new', { errorMsg: err.message }); SKIP old code
+    res.status(500).json({ err });
+  }
+}
+
+async function loginDetails(req, res) {
+  try {
+    const user = await Users.loginDetails(req.query);
+    // Always redirect after CUDing data
+    // We'll refactor to redirect to the movies index after we implement it
+    // res.redirect('/movies/new'); SKIP old code
+    res.json(user);
+  } catch (err) {
+    // Typically some sort of validation error
+    console.log(err);
+    // res.render('movies/new', { errorMsg: err.message }); SKIP old code
+    res.status(500).json({ err });
+  }
+}
+
+async function loginUser(req, res) {
+  try {
+    const token = await Users.loginUser(req.body);
+    console.log(token);
+    if (!token.success) {
+      res.status(400).json({ errorMsg: token.error });
+      return;
     }
+    res.json(token.data);
+  } catch (err) {
+    res.status(500).json({ errorMsg: err.message });
   }
+}
 
-  async function loginDetails(req, res) {
-    try {
-      const user = await Users.loginDetails(req.query);
-      // Always redirect after CUDing data
-      // We'll refactor to redirect to the movies index after we implement it
-      // res.redirect('/movies/new'); SKIP old code
-      res.json(user)
-    } catch (err) {
-      // Typically some sort of validation error
-      console.log(err);
-      // res.render('movies/new', { errorMsg: err.message }); SKIP old code
-      res.status(500).json({ err });
+function checkLogin(req, res) {
+  res.json({ user: req.user });
+}
+
+function checkPermission(req, res) {
+  res.json({ user: req.user, body: req.body.email });
+}
+
+async function logoutUser(req, res) {
+  try {
+    const result = await Users.logoutUser(req.body);
+    if (!result.success) {
+      res.status(400).json({ errorMsg: result.error });
+      return;
     }
+    res.json(result.data);
+  } catch (err) {
+    res.status(500).json({ errorMsg: err.message });
   }
+}
 
-  async function loginUser(req, res) {
-    try {
-        const token = await Users.loginUser(req.body);
-        console.log(token);
-        if (!token.success) {
-          res.status(400).json({errorMsg: token.error})
-          return 
-        }
-        res.json(token.data)
-    } catch (err) {
-        res.status(500).json({ errorMsg: err.message });
+async function createOrder(req, res) {
+  try {
+    // Access the authenticated user information
+    const userEmail = req.user.payload.email;
+
+    // Combine user information with order data
+    const orderData = {
+      ...req.body,
+      userEmail: userEmail,
+    };
+
+    const order = await Users.createOrder(orderData);
+    res.json(order);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+}
+
+async function updateOrder(req, res) {
+  const { orderId } = req.params;
+  const updateData = req.body;
+
+  try {
+    const result = await Users.updateOrder(orderId, updateData);
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+}
+
+// async function orderDetails(req, res) {
+//   try {
+//     const order = await Users.orderDetails(req.query);
+//     // Always redirect after CUDing data
+//     // We'll refactor to redirect to the movies index after we implement it
+//     // res.redirect('/movies/new'); SKIP old code
+//     res.json(order)
+//   } catch (err) {
+//     // Typically some sort of validation error
+//     console.log(err);
+//     // res.render('movies/new', { errorMsg: err.message }); SKIP old code
+//     res.status(500).json({ err });
+//   }
+// }
+
+async function orderDetails(req, res) {
+  try {
+    // Extract user email from the authenticated user object
+    const userEmail = req.user.payload.email;
+
+    // Call the orderDetails function with query parameters and user email
+    const result = await Users.orderDetails(req.query, userEmail);
+
+    // Check the result and send appropriate response
+    if (result.success) {
+      res.json(result);
+    } else {
+      // If the order wasn't found or doesn't belong to the user, send a 404 status
+      res.status(404).json(result);
     }
+  } catch (err) {
+    console.error("Error in orderDetails controller:", err);
+    res.status(500).json({
+      success: false,
+      error: "An error occurred while retrieving order details",
+    });
   }
+}
 
-  function checkLogin(req, res) {
-    res.json({user: req.user});
+async function productDetails(req, res) {
+  try {
+    const product = await Users.productDetails(req.query);
+
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err });
   }
+}
 
-  function checkPermission(req, res) {
-    res.json({user: req.user, body: req.body.email});
+async function createProduct(req, res) {
+  try {
+    const product = await daoProduct.create(req.body);
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
   }
+}
 
-  async function logoutUser(req, res) {
-    try {
-        const result = await Users.logoutUser(req.body);
-        if (!result.success) {
-          res.status(400).json({errorMsg: result.error})
-          return 
-        }
-        res.json(result.data)
-    } catch (err) {
-        res.status(500).json({ errorMsg: err.message });
-    }
+async function allProductDetails(req, res) {
+  try {
+    const product = await Users.allProductDetails(req.query);
+
+    res.json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err });
   }
+}
 
-  async function createOrder(req, res) {
-    try {
-      // Access the authenticated user information
-      const userEmail = req.user.payload.email;
-      
-      // Combine user information with order data
-      const orderData = {
-        ...req.body,
-        userEmail: userEmail
-      };
-  
-      const order = await Users.createOrder(orderData);
-      res.json(order);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: err.message });
-    }
+async function allOrderDetails(req, res) {
+  try {
+    const orders = await Users.allOrderDetails(req.query);
+
+    res.json(orders);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err });
   }
+}
 
-  async function updateOrder(req, res) {
-
-    const { orderId } = req.params;
+async function updateProduct(req, res) {
+  try {
+    const productId = req.params.productId;
     const updateData = req.body;
-
-    try {
-       const result = await Users.updateOrder(orderId, updateData);
-       res.json(result);
-     } catch (err) {
-       console.log(err);
-       res.status(500).json({ error: err.message });
-     }
+    const updatedProduct = await Users.updateProduct(productId, updateData);
+    res.json(updatedProduct);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
   }
-
-  // async function orderDetails(req, res) {
-  //   try {
-  //     const order = await Users.orderDetails(req.query);
-  //     // Always redirect after CUDing data
-  //     // We'll refactor to redirect to the movies index after we implement it
-  //     // res.redirect('/movies/new'); SKIP old code
-  //     res.json(order)
-  //   } catch (err) {
-  //     // Typically some sort of validation error
-  //     console.log(err);
-  //     // res.render('movies/new', { errorMsg: err.message }); SKIP old code
-  //     res.status(500).json({ err });
-  //   }
-  // }
-
-  async function orderDetails(req, res) {
-    try {
-      // Extract user email from the authenticated user object
-      const userEmail = req.user.payload.email;
-  
-      // Call the orderDetails function with query parameters and user email
-      const result = await Users.orderDetails(req.query, userEmail);
-  
-      // Check the result and send appropriate response
-      if (result.success) {
-        res.json(result);
-      } else {
-        // If the order wasn't found or doesn't belong to the user, send a 404 status
-        res.status(404).json(result);
-      }
-    } catch (err) {
-      console.error('Error in orderDetails controller:', err);
-      res.status(500).json({ 
-        success: false, 
-        error: 'An error occurred while retrieving order details'
-      });
-    }
-  }
-  
-
-  async function productDetails(req, res) {
-    try {
-      const product = await Users.productDetails(req.query);
-
-      res.json(product)
-      } catch (err) {
-        console.log(err);
-        res.status(500).json({ err });
-        }
-      }
-
-
-      async function createProduct(req, res) {
-        try {
-          const product = await daoProduct.create(req.body);
-          res.json(product);
-        } catch (err) {
-          console.log(err);
-          res.status(500).json({ error: err.message });
-        }
-    }
-
-  async function allProductDetails(req, res) {
-    try {
-      const product = await Users.allProductDetails(req.query);
-
-      res.json(product)
-      } catch (err) {
-        console.log(err);
-        res.status(500).json({ err });
-        }
-      }
-
-
-  async function allOrderDetails(req, res) {
-    try {
-      const orders = await Users.allOrderDetails(req.query);
-
-      res.json(orders)
-      } catch (err) {
-        console.log(err);
-        res.status(500).json({ err });
-        }
-      }
-    
-
-      async function updateProduct(req, res) {
-        try {
-          const productId = req.params.productId;
-          const updateData = req.body;
-          const updatedProduct = await daoProduct.update(productId, updateData);
-          res.json(updatedProduct);
-        } catch (err) {
-          console.log(err);
-          res.status(500).json({ error: err.message });
-        }
-      }
+}
--- models/users.js
+++ models/users.js
@@ -1,170 +1,181 @@
 const daoUser = require("../daos/users");
-const utilSecurity = require("../util/security")
+const utilSecurity = require("../util/security");
 const daoOrder = require("../daos/orders");
-const daoProduct =  require("../daos/product");
-const crypto = require('crypto');
+const daoProduct = require("../daos/product");
+const crypto = require("crypto");
 
 module.exports = {
-    signup,
-    loginDetails,
-    loginUser,
-    logoutUser,
-    createOrder,
-    orderDetails,
-    updateOrder,
-    productDetails,
-    createProduct,
-    allProductDetails,
-    allOrderDetails,
-    update
-    
-}
+  signup,
+  loginDetails,
+  loginUser,
+  logoutUser,
+  createOrder,
+  orderDetails,
+  updateOrder,
+  productDetails,
+  createProduct,
+  allProductDetails,
+  allOrderDetails,
+  updateProduct,
+};
 
-// [a,b,c] vs {a:a, b:b, c:c} 
-// => list.filter(a) // go through all 
+// [a,b,c] vs {a:a, b:b, c:c}
+// => list.filter(a) // go through all
 // object.find(a) // only check the a drawer
 // usertypes ("admin", "normal")
 // admin -> list of admins
 
 function generateUniqueOrderID() {
   const date = new Date();
-  const MM = String(date.getMonth() + 1).padStart(2, '0');
-  const DD = String(date.getDate()).padStart(2, '0');
-  const SSS = String(date.getMilliseconds()).padStart(3, '0');
-  
+  const MM = String(date.getMonth() + 1).padStart(2, "0");
+  const DD = String(date.getDate()).padStart(2, "0");
+  const SSS = String(date.getMilliseconds()).padStart(3, "0");
+
   const formattedDate = `${MM}${DD}${SSS}`;
-  const randomString = crypto.randomBytes(2).toString('hex'); // Generates a 4-character hex string
+  const randomString = crypto.randomBytes(2).toString("hex"); // Generates a 4-character hex string
   return `${formattedDate}${randomString}`;
 }
 
 async function signup(body) {
-    //
-    const user = await daoUser.findOne({"email": body.email});
-    console.log(user);
-    if (user) {
-      return {success: false, error: "user already exist"};
-    }
-    const newUser = await daoUser.create(body);
-    return {success: true, data: newUser};
+  //
+  const user = await daoUser.findOne({ email: body.email });
+  console.log(user);
+  if (user) {
+    return { success: false, error: "user already exist" };
   }
+  const newUser = await daoUser.create(body);
+  return { success: true, data: newUser };
+}
 
 async function loginDetails(body) {
-    //
-    const loginDetailsSchema = {
-      "email": 1,
-      "salt": 1,
-      "iterations": 1
-    }
-    console.log(body.email)
-    const user = await daoUser.findOne({"email": body.email}, loginDetailsSchema);
-
-    console.log(user);
-    return {success: true, data: user};
-  }
-
-  async function loginUser(body) {
-    if (!body.hasOwnProperty("email")) {
-      return {success: false, error: "missing email"};
-    }
-    if (!body.hasOwnProperty("password")) {
-      return {success: false, error: "missing password"};
-    }
-  
-    const user = await daoUser.findOne({"email": body.email, "password": body.password});
-    if (user == null || Object.keys(user).length == 0) {
-      return {success: false, error: "Invalid email/password"};
-    }
-
-    const jwtPayload = {
-      user: user.firstName,
-      email: user.email,
-      is_admin: user.is_admin
-    };
-    const token = utilSecurity.createJWT(jwtPayload);
-    const expiry = utilSecurity.getExpiry(token);
-    await daoUser.updateOne({"email": body.email}, {token: token, expire_at: expiry})
-    return {success: true, data: token}
-  }
-
-  async function logoutUser(body) {
-    if (!body.hasOwnProperty('email')) {
-      return {success: false, error: "missing email"};
-    }
-    await daoUser.updateOne({"email": body.email}, {token: null, expire_at: null})
-    return {success: true, data: "logout successful!"}
-  }
-
-  // async function createOrder(body) {
-  //   const order = await daoOrder.findOne({"order": body.orderID});
-  //   console.log(order);
-  //   if (order) {
-  //     return {success: false, error: "order already exist"};
-  //   }
-  //   const newOrder = await daoOrder.create(body);
-  //   return {success: true, data: newOrder};
-  // }
-
-  async function createOrder(orderData) {
-    try {
-      console.log(orderData);
-      const { userEmail, ...orderDetails } = orderData;
-
-      // Generate a unique order ID
-      orderDetails.orderID = await generateUniqueOrderID();
-      console.log(orderDetails);
-
-  
-      // Check if the order already exists
-      // const existingOrder = await daoOrder.findOne({ orderID: orderDetails.orderID });
-  
-      // if (existingOrder) {
-      //   throw new Error('Order already exists');
-      // }
-  
-      // Create the order
-      const newOrder = await daoOrder.create(orderDetails);
-  
-      // Find the user by email
-      const user = await daoUser.findOne({ email: userEmail });
-  
-      if (!user) {
-        throw new Error('User not found');
-      }
-  
-      // Attach the created order's ID to the user and update the user
-      user.order.push(newOrder._id);
-      await user.save();
-  
-      // Return the new order
-      return { success: true, data: newOrder };
-    } catch (err) {
-      console.error('Error creating order:', err);
-      return { success: false, error: err.message };
-    }
-  }
-
-
-  async function updateOrder(orderId, body) {
-    try {
-       const updatedOrder = await daoOrder.findByIdAndUpdate(orderId, body, { new: true }).populate({
-        path: 'drinks.mainProduct',
-        select: 'name category price inStock'
-      }).populate({
-        path: 'drinks.toppings.topping',
-        select: 'name category price inStock'
-      });
-  
-      if (!updatedOrder) {
-        return { success: false, error: 'Order not found' };
-      }
-  
-      return { success: true, data: updatedOrder };
-    } catch (err) {
-      console.error('Error updating order:', err);
-      return { success: false, error: 'An error occurred while updating the order' };
-    }
+  //
+  const loginDetailsSchema = {
+    email: 1,
+    salt: 1,
+    iterations: 1,
   };
+  console.log(body.email);
+  const user = await daoUser.findOne({ email: body.email }, loginDetailsSchema);
 
+  console.log(user);
+  return { success: true, data: user };
+}
+
+async function loginUser(body) {
+  if (!body.hasOwnProperty("email")) {
+    return { success: false, error: "missing email" };
+  }
+  if (!body.hasOwnProperty("password")) {
+    return { success: false, error: "missing password" };
+  }
+
+  const user = await daoUser.findOne({
+    email: body.email,
+    password: body.password,
+  });
+  if (user == null || Object.keys(user).length == 0) {
+    return { success: false, error: "Invalid email/password" };
+  }
+
+  const jwtPayload = {
+    user: user.firstName,
+    email: user.email,
+    is_admin: user.is_admin,
+  };
+  const token = utilSecurity.createJWT(jwtPayload);
+  const expiry = utilSecurity.getExpiry(token);
+  await daoUser.updateOne(
+    { email: body.email },
+    { token: token, expire_at: expiry }
+  );
+  return { success: true, data: token };
+}
+
+async function logoutUser(body) {
+  if (!body.hasOwnProperty("email")) {
+    return { success: false, error: "missing email" };
+  }
+  await daoUser.updateOne(
+    { email: body.email },
+    { token: null, expire_at: null }
+  );
+  return { success: true, data: "logout successful!" };
+}
+
+// async function createOrder(body) {
+//   const order = await daoOrder.findOne({"order": body.orderID});
+//   console.log(order);
+//   if (order) {
+//     return {success: false, error: "order already exist"};
+//   }
+//   const newOrder = await daoOrder.create(body);
+//   return {success: true, data: newOrder};
+// }
+
+async function createOrder(orderData) {
+  try {
+    console.log(orderData);
+    const { userEmail, ...orderDetails } = orderData;
+
+    // Generate a unique order ID
+    orderDetails.orderID = await generateUniqueOrderID();
+    console.log(orderDetails);
+
+    // Check if the order already exists
+    // const existingOrder = await daoOrder.findOne({ orderID: orderDetails.orderID });
+
+    // if (existingOrder) {
+    //   throw new Error('Order already exists');
+    // }
+
+    // Create the order
+    const newOrder = await daoOrder.create(orderDetails);
+
+    // Find the user by email
+    const user = await daoUser.findOne({ email: userEmail });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Attach the created order's ID to the user and update the user
+    user.order.push(newOrder._id);
+    await user.save();
+
+    // Return the new order
+    return { success: true, data: newOrder };
+  } catch (err) {
+    console.error("Error creating order:", err);
+    return { success: false, error: err.message };
+  }
+}
+
+async function updateOrder(orderId, body) {
+  try {
+    const updatedOrder = await daoOrder
+      .findByIdAndUpdate(orderId, body, { new: true })
+      .populate({
+        path: "drinks.mainProduct",
+        select: "name category price inStock",
+      })
+      .populate({
+        path: "drinks.toppings.topping",
+        select: "name category price inStock",
+      });
+
+    if (!updatedOrder) {
+      return { success: false, error: "Order not found" };
+    }
+
+    return { success: true, data: updatedOrder };
+  } catch (err) {
+    console.error("Error updating order:", err);
+    return {
+      success: false,
+      error: "An error occurred while updating the order",
+    };
+  }
+}
 
 // Function to get order details
 // async function orderDetails(body) {
@@ -190,17 +201,17 @@ async function loginDetails(body) {
 
 async function orderDetails(body, userEmail) {
   const orderDetailsSchema = {
-    "orderID": 1,
-    "comment": 1,
-    "total": 1,
-    "status": 1,
-    "createdAt": 1,
-    "updatedAt":1,
-    "created_at": 1,
-    "expire_at": 1,
-    "is_paid": 1,
+    orderID: 1,
+    comment: 1,
+    total: 1,
+    status: 1,
+    createdAt: 1,
+    updatedAt: 1,
+    created_at: 1,
+    expire_at: 1,
+    is_paid: 1,
     "drinks.mainProduct": 1,
-    "drinks.quantity":1,
+    "drinks.quantity": 1,
     "drinks.toppings.topping": 1,
     "drinks.toppings.quantity": 1,
     "drinks.comment": 1,
@@ -215,112 +226,132 @@ async function orderDetails(body, userEmail) {
     }
 
     // Find the order that belongs to the user
-    const order = await daoOrder.find({ 
-      "_id": { $in: user.order } // Assuming user.order is an array of order IDs
-    }, orderDetailsSchema).populate({
-      path: 'drinks.mainProduct',
-      select: 'name category price inStock',
-      options: { strictPopulate: false }
-    }).populate({
-      path: 'drinks.toppings.topping',
-      select: 'name category price inStock',
-      options: { strictPopulate: false }
-    });
+    const order = await daoOrder
+      .find(
+        {
+          _id: { $in: user.order }, // Assuming user.order is an array of order IDs
+        },
+        orderDetailsSchema
+      )
+      .populate({
+        path: "drinks.mainProduct",
+        select: "name category price inStock",
+        options: { strictPopulate: false },
+      })
+      .populate({
+        path: "drinks.toppings.topping",
+        select: "name category price inStock",
+        options: { strictPopulate: false },
+      });
 
     if (!order) {
-      return { success: false, error: "Order not found or does not belong to the user" };
+      return {
+        success: false,
+        error: "Order not found or does not belong to the user",
+      };
     }
 
     return { success: true, data: order };
   } catch (err) {
-    console.error('Error retrieving order details:', err);
-    return { success: false, error: "An error occurred while retrieving order details" };
+    console.error("Error retrieving order details:", err);
+    return {
+      success: false,
+      error: "An error occurred while retrieving order details",
+    };
   }
 }
-
 
 async function createProduct(productData) {
   try {
-      const product = await daoProduct.create(productData);
-      return { success: true, data: product };
+    const product = await daoProduct.create(productData);
+    return { success: true, data: product };
   } catch (err) {
-      return { success: false, error: err.message };
+    return { success: false, error: err.message };
   }
 }
 
+async function productDetails(body) {
+  const productDetailsSchema = {
+    name: 1,
+    price: 1,
+    category: 1,
+    inStock: 1,
+  };
+  const product = await daoProduct.findOne(
+    { name: body.name },
+    productDetailsSchema
+  );
+  if (!product) {
+    return { success: false, error: "Product not found" };
+  }
+  return { success: true, data: product };
+}
 
-  async function productDetails(body) {
-    const productDetailsSchema = {
-      "name": 1,
-      "price": 1,
-      "category": 1,
-      "inStock": 1,}
-      const product = await daoProduct.findOne({ "name": body.name }, productDetailsSchema);
-       if (!product) {
-        return { success: false, error: "Product not found" };
-    }
-    return { success: true, data: product };
+async function allProductDetails() {
+  const productDetailsSchema = {
+    name: 1,
+    price: 1,
+    category: 1,
+    inStock: 1,
+  };
+
+  const products = await daoProduct.find({}, productDetailsSchema);
+
+  if (!products || products.length === 0) {
+    return { success: false, error: "No products found" };
   }
 
-  async function allProductDetails() {
-    const productDetailsSchema = {
-      "name": 1,
-      "price": 1,
-      "category": 1,
-      "inStock": 1
-    };
-    
-    const products = await daoProduct.find({}, productDetailsSchema);
-    
-    if (!products || products.length === 0) {
-      return { success: false, error: "No products found" };
-    }
-    
-    return { success: true, data: products };
-  }
+  return { success: true, data: products };
+}
 
-  async function allOrderDetails() {
-    const orderDetailsSchema = {
-      "orderID": 1,
-      "comment": 1,
-      "total": 1,
-      "status": 1,
-      "createdAt": 1,
-      "updatedAt":1,
-      "created_at": 1,
-      "expire_at": 1,
-      "is_paid": 1,
-      "drinks.mainProduct": 1,
-      "drinks.quantity":1,
-      "drinks.toppings.topping": 1,
-      "drinks.toppings.quantity": 1,
-      "drinks.comment": 1,
-    };
-    
-    const orders = await daoOrder.find({}, orderDetailsSchema).populate({
-      path: 'drinks.mainProduct',
-      select: 'name category price inStock',
-      options: { strictPopulate: false }
-    }).populate({
-      path: 'drinks.toppings.topping',
-      select: 'name category price inStock',
-      options: { strictPopulate: false }
+async function allOrderDetails() {
+  const orderDetailsSchema = {
+    orderID: 1,
+    comment: 1,
+    total: 1,
+    status: 1,
+    createdAt: 1,
+    updatedAt: 1,
+    created_at: 1,
+    expire_at: 1,
+    is_paid: 1,
+    "drinks.mainProduct": 1,
+    "drinks.quantity": 1,
+    "drinks.toppings.topping": 1,
+    "drinks.toppings.quantity": 1,
+    "drinks.comment": 1,
+  };
+
+  const orders = await daoOrder
+    .find({}, orderDetailsSchema)
+    .populate({
+      path: "drinks.mainProduct",
+      select: "name category price inStock",
+      options: { strictPopulate: false },
+    })
+    .populate({
+      path: "drinks.toppings.topping",
+      select: "name category price inStock",
+      options: { strictPopulate: false },
     });
 
-    
-    if (!orders || orders.length === 0) {
-      return { success: false, error: "No orders found" };
-    }
-    
-    return { success: true, data: orders };
+  if (!orders || orders.length === 0) {
+    return { success: false, error: "No orders found" };
   }
 
-  async function update(productId, updateData) {
-    try {
-      const updatedProduct = await Product.findByIdAndUpdate(productId, updateData, { new: true });
-      return updatedProduct;
-    } catch (err) {
-      console.error('Error updating product:', err);
-      throw err;
-    }
+  return { success: true, data: orders };
+}
+
+async function updateProduct(productId, updateData) {
+  try {
+    const updatedProduct = await daoProduct.findByIdAndUpdate(
+      productId,
+      updateData,
+      { new: true }
+    );
+    return updatedProduct;
+  } catch (err) {
+    console.error("Error updating product:", err);
+    throw err;
   }
+}
